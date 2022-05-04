81349db5f54648a012319423096f0c769fe3b8ae
Michael <michaelnji34@gmail.com>
2022-05-04 15:16:02 +0100

    ✨ setting up api management

--- api/blog.js
+++ api/blog.js
@@ -8,7 +8,6 @@ export default function fetchUser(req, res) {
     accessToken: process.env.CONTENTFUL_API_KEY,
   });
 
-  var blogPosts;
   client
     .getEntries()
     .then(function (entries) {
@@ -20,7 +19,7 @@ export default function fetchUser(req, res) {
     .catch(function (error) {
       res.send({
         success: false,
-        data: undefined,
+        data: error,
       });
       console.log("Error occurred : ", error);
     });
